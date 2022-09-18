cd6e522e813d22d11f84e5604e64b265d6b11885
Kyle Nelli <knelli@caltech.edu>
2022-09-18 15:21:35 -0700

    Add global quantities in file

---
+++ src/Global.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = Path(SRC_DIR).parents[0]
+
+VERSION_FILENAME = "VERSIONS"
+DEFAULT_VERSION_DICT = {"Files": {}, "Collective": 0}
+
+DB_DIR = os.path.join(ROOT_DIR, "db")
+
+RAW_STATEMENT_DIR = os.path.join(DB_DIR, "raw_earning_statements")
+CLEAN_STATEMENT_DIR = os.path.join(DB_DIR, "clean_earning_statements")
+CLEAN_STATEMENT_DB = os.path.join(CLEAN_STATEMENT_DIR, "db.json")
