d2dcb9bb8f52a2fe62057ac58d824cff2f72e127
Kyle Nelli <kcnelli21@gmail.com>
2022-10-08 17:10:54 -0700

    Add more mongo utils

--- src/mongo/GetClient.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from Global import *
-
-def get_client():
-  set_secrets()
-  return pymongo.MongoClient(
-      f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}"
-      "@plmanalytics.uwia1aq.mongodb.net/$retryWrites=true&w=majority"
-  )
